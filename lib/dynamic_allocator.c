6297cf059adac4d39efe8c530119e68b711232e1
belalzz <belal.khaled041@gmail.com>
2026-02-14 01:38:17 +0200

    refactored dynamic allocator

--- lib/dynamic_allocator.c
+++ lib/dynamic_allocator.c
@@ -12,43 +12,48 @@
 //==================================================================================//
 //============================== GIVEN FUNCTIONS ===================================//
 //==================================================================================//
+
 //==================================
-//==================================
-// [1] GET PAGE VA:
+// [1] GET PAGE VA
 //==================================
 __inline__ uint32 to_page_va(struct PageInfoElement *ptrPageInfo)
 {
-	if (ptrPageInfo < &pageBlockInfoArr[0] || ptrPageInfo >= &pageBlockInfoArr[DYN_ALLOC_MAX_SIZE/PAGE_SIZE])
-			panic("to_page_va called with invalid pageInfoPtr");
-	//Get start VA of the page from the corresponding Page Info pointer
-	int idxInPageInfoArr = (ptrPageInfo - pageBlockInfoArr);
-	return dynAllocStart + (idxInPageInfoArr << PGSHIFT);
+    if (ptrPageInfo < &pageBlockInfoArr[0] ||
+        ptrPageInfo >= &pageBlockInfoArr[DYN_ALLOC_MAX_SIZE / PAGE_SIZE])
+        panic("to_page_va called with invalid pageInfoPtr");
+
+    int idxInPageInfoArr = (ptrPageInfo - pageBlockInfoArr);
+    return dynAllocStart + (idxInPageInfoArr << PGSHIFT);
 }
 
 //==================================
-// [2] GET PAGE INFO OF PAGE VA:
+// [2] GET PAGE INFO OF PAGE VA
 //==================================
 __inline__ struct PageInfoElement * to_page_info(uint32 va)
 {
-	int idxInPageInfoArr = (va - dynAllocStart) >> PGSHIFT;
-	if (idxInPageInfoArr < 0 || idxInPageInfoArr >= DYN_ALLOC_MAX_SIZE/PAGE_SIZE)
-		panic("to_page_info called with invalid pa");
-	return &pageBlockInfoArr[idxInPageInfoArr];
+    int idxInPageInfoArr = (va - dynAllocStart) >> PGSHIFT;
+    if (idxInPageInfoArr < 0 || idxInPageInfoArr >= DYN_ALLOC_MAX_SIZE / PAGE_SIZE)
+        panic("to_page_info called with invalid va");
+
+    return &pageBlockInfoArr[idxInPageInfoArr];
 }
 
 //==================================================================================//
 //============================ REQUIRED FUNCTIONS ==================================//
 //==================================================================================//
-//=====================//
-// h) get el index for suitable size
-//=====================//
-int get_idx_by_size(uint32 size){
-	 int idx=0;
-	for (int i=LOG2_MIN_SIZE; i<=LOG2_MAX_SIZE ; ++i) {
-		if((1 << i) >= size) break;
-		 idx++;
-	}
-	return idx;
+
+//===========================
+// h) Get index by block size
+//===========================
+int get_idx_by_size(uint32 size)
+{
+    int idx = 0;
+    for (int i = LOG2_MIN_SIZE; i <= LOG2_MAX_SIZE; ++i)
+    {
+        if ((1 << i) >= size) break;
+        idx++;
+    }
+    return idx;
 }
 
 
@@ -70,32 +75,26 @@ void initialize_dynamic_allocator(uint32 daStart, uint32 daEnd)
 	//==================================================================================
 	//TODO: [PROJECT'25.GM#1] DYNAMIC ALLOCATOR - #1 initialize_dynamic_allocator
 
-	//setup el bounds bta3et el dynamicallocator.
-	dynAllocStart= daStart;
-	dynAllocEnd= daEnd;
+	dynAllocStart = daStart;
+	dynAllocEnd = daEnd;
 
-	// binit el freePages 3shan add feha el available pageBlockInfo ..
+	// Initialize free pages list
 	LIST_INIT(&freePagesList);
 
-	// init the array of lists of each size..
-	// 8 16 32 64 128 256 512 1024 2056
-	// 0 1  2  3   4  5    6   7    8
-	// hninit kol list fel freeblocklists by iterating over each size..
-	for (int i=0; i<=(LOG2_MAX_SIZE-LOG2_MIN_SIZE);++i){
+	// Initialize free block lists for each size
+	for (int i = 0; i <= (LOG2_MAX_SIZE - LOG2_MIN_SIZE); ++i)
+	{
 		LIST_INIT(&freeBlockLists[i]);
 	}
 
-	// 3dad el pages fel dynamic bet el bounds.
-	int pCount =(daEnd-daStart)/PAGE_SIZE;
-	// hniterate 3la kol pageBlockInfo w n resetaha w push it fel free Pages list.
-	for (int i =0;i<pCount; ++i) {
-	     pageBlockInfoArr[i].block_size=0;
-	     pageBlockInfoArr[i].num_of_free_blocks=0;
-	     LIST_INSERT_TAIL(&freePagesList,&pageBlockInfoArr[i]);
+	// Initialize page info array and add pages to freePagesList
+	int pCount = (daEnd - daStart) / PAGE_SIZE;
+	for (int i = 0; i < pCount; ++i)
+	{
+		pageBlockInfoArr[i].block_size = 0;
+		pageBlockInfoArr[i].num_of_free_blocks = 0;
+		LIST_INSERT_TAIL(&freePagesList, &pageBlockInfoArr[i]);
 	}
-	//Comment the following line
-	//panic("initialize_dynamic_allocator() Not implemented yet");
-
 }
 
 //===========================
@@ -105,40 +104,33 @@ __inline__ uint32 get_block_size(void *va)
 {
 	//TODO: [PROJECT'25.GM#1] DYNAMIC ALLOCATOR - #2 get_block_size
 	//Your code is here
-	// bel virtual address hn get el page info and by el pointer return blk-size. .
+
 	struct PageInfoElement* pg_t=to_page_info((uint32)va);
-	// cprintf("%x\n",(uint32)va);
 	return pg_t->block_size;
-	//Comment the following line
 	//panic("get_block_size() Not implemented yet");
 }
 
 // =====================
 // h) split page to blocks and add them in freeBlockLists
 // =====================
-// function that split the page provided into same sized blocks
-//and putem in suitable freeblockList index according to size.
- void split_page_to_blocks(uint32 blk_size, struct PageInfoElement *pageInfo) {
-	 //from the pageInfo we get the virtual address. .
-	 //w hnsave page in memory.
-	 uint32 pgva=(uint32)to_page_va(pageInfo);
-	 get_page((uint32*)pgva);
 
-	 // el blk count ely fel page.
-	 int cblk=PAGE_SIZE/blk_size;
+void split_page_to_blocks(uint32 blk_size, struct PageInfoElement *pageInfo) {
+	uint32 pg_va = to_page_va(pageInfo);
+	get_page((uint32 *)pg_va);
 
-	 //bnassign el new values fe pageInfo.
-	 pageInfo->num_of_free_blocks=cblk;
-	 pageInfo->block_size = blk_size;
-	 //get el index lel size ..
-	 int idx=get_idx_by_size(blk_size);
-	 // iterate over each blk and make and element of it by passing its address in blockElement pointer and save those in the list.
-	 // starting from the pgva.
-	 for(uint32 i=0; i<cblk;++i) {
-		 struct BlockElement *b_toA=(struct BlockElement*)(pgva+ i*blk_size);
-		 // addign to the list fo suitable size. .
-		 LIST_INSERT_TAIL(&freeBlockLists[idx],b_toA);
-	 }
+	int num_blocks = PAGE_SIZE / blk_size;
+
+	pageInfo->num_of_free_blocks = num_blocks;
+	pageInfo->block_size = blk_size;
+
+	int idx = get_idx_by_size(blk_size);
+
+	for (uint32 i = 0; i < num_blocks; ++i)
+	{
+		struct BlockElement *blk = (struct BlockElement *)(pg_va + i * blk_size);
+		LIST_INSERT_TAIL(&freeBlockLists[idx], blk);
+	}
+
  }
 
 
@@ -157,59 +149,52 @@ void *alloc_block(uint32 size)
 	//==================================================================================
 	//TODO: [PROJECT'25.GM#1] DYNAMIC ALLOCATOR - #3 alloc_block
 	//Your code is here
-	//Comment the following line
-	//hngeb suitable index for the size..
 	int idx = get_idx_by_size(size);
 
-	//((case1):el blk bel size is available)
-	if (freeBlockLists[idx].size > 0) {
-		// remove from the list and updat the pageInfoArr free blocks.
-		// pop from the freeBlockList.
-		struct BlockElement *resBlk=LIST_FIRST(&freeBlockLists[idx]);
-	    LIST_REMOVE(&freeBlockLists[idx], resBlk);
-	    // update pageBlockInfoArr
-	    struct PageInfoElement *pgn = to_page_info((uint32)resBlk);
-	    pgn->num_of_free_blocks--;
-	    return resBlk;
-	}else{///(case2)mfesh free block bs there is available pages.
-			//(case3)mfesh freeblks wla pages so-> try sizeup el blk.
-		if(freePagesList.size > 0){ //have free pages(case2).
-			// get page to split and use from freepagesList.
-			struct PageInfoElement *pgn=LIST_FIRST(&freePagesList);
-			 LIST_REMOVE(&freePagesList,pgn);
-			//from page to freeblockList by the pageInfo and the block size.
-			 split_page_to_blocks(1<<(idx+LOG2_MIN_SIZE),pgn);
-			// remove from the list and updat the pageInfoArr free blocks.
-			// pop from the freeBlockList.
-			struct BlockElement *resBlk=LIST_FIRST(&freeBlockLists[idx]);
-		    LIST_REMOVE(&freeBlockLists[idx],resBlk);
-		    // update pageBlockInfoArr
-		    struct PageInfoElement *ppgn=to_page_info((uint32)resBlk);
-		    ppgn->num_of_free_blocks--;
-		    return resBlk;
-		}else{//(case3):
-			// hnincrease el size of power2 as smaller than maxsize and no av. blocks
-			while (idx<=(LOG2_MAX_SIZE-LOG2_MIN_SIZE)&&freeBlockLists[idx].size==0) {
-				idx++;
-				//lw found free block
-				if(freeBlockLists[idx].size>0){
-				// remove from the list and updat the pageInfoArr free blocks.
-				// pop from the freeBlockList.
-					struct BlockElement *resBlk = LIST_FIRST(&freeBlockLists[idx]);
-					LIST_REMOVE(&freeBlockLists[idx], resBlk);
-				// update pageBlockInfoArr
-					struct PageInfoElement *pgn=to_page_info((uint32)resBlk);
-					pgn->num_of_free_blocks--;
-					return resBlk;
-				}
-			}
-		}
+	// Case 1: free block available in requested size
+	if (freeBlockLists[idx].size > 0)
+	{
+		struct BlockElement *blk = LIST_FIRST(&freeBlockLists[idx]);
+		LIST_REMOVE(&freeBlockLists[idx], blk);
+
+		struct PageInfoElement *pg = to_page_info((uint32)blk);
+		pg->num_of_free_blocks--;
+		return blk;
 	}
-	//(case4)lw mfesh el etnin so panic..
+
+	// Case 2: no free block but free pages available
+	if (freePagesList.size > 0)
+	{
+		struct PageInfoElement *pg = LIST_FIRST(&freePagesList);
+		LIST_REMOVE(&freePagesList, pg);
+
+		split_page_to_blocks(1 << (idx + LOG2_MIN_SIZE), pg);
+
+		struct BlockElement *blk = LIST_FIRST(&freeBlockLists[idx]);
+		LIST_REMOVE(&freeBlockLists[idx], blk);
+
+		struct PageInfoElement *pg_res = to_page_info((uint32)blk);
+		pg_res->num_of_free_blocks--;
+		return blk;
+	}
+
+	// Case 3: try larger block sizes
+	while (idx <= (LOG2_MAX_SIZE - LOG2_MIN_SIZE))
+	{
+		if (freeBlockLists[idx].size > 0)
+		{
+			struct BlockElement *blk = LIST_FIRST(&freeBlockLists[idx]);
+			LIST_REMOVE(&freeBlockLists[idx], blk);
+
+			struct PageInfoElement *pg = to_page_info((uint32)blk);
+			pg->num_of_free_blocks--;
+			return blk;
+		}
+		idx++;
+	}
+
+	// Case 4: no free blocks/pages available
 	panic("no free blocks!");
-	//panic("alloc_block() Not implemented yet");
-	//TODO: [PROJECT'25.BONUS#1] DYNAMIC ALLOCATOR - block if no free block
-	//sleep(dChannel_blocked);
 	return NULL;
 }
 
@@ -229,38 +214,31 @@ void free_block(void *va)
 
 	//TODO: [PROJECT'25.GM#1] DYNAMIC ALLOCATOR - #4 free_block
 	//Your code is here
-	//Comment the following line
-	//bel virtual address hngeb el page wel block wl size..
-	struct BlockElement* frBlk=(struct BlockElement*)va;
-	struct PageInfoElement *pgf=to_page_info((uint32)va);
-	uint32 blk_size = pgf->block_size;
-
-	// its index in the freeBlocklist.
+	struct BlockElement *blk = (struct BlockElement *)va;
+	struct PageInfoElement *pg = to_page_info((uint32)va);
+	uint32 blk_size = pg->block_size;
 	int idx = get_idx_by_size(blk_size);
 
-	//adding el block lel page
-	pgf->num_of_free_blocks++;
-	LIST_INSERT_TAIL(&freeBlockLists[idx], frBlk);
+	pg->num_of_free_blocks++;
+	LIST_INSERT_TAIL(&freeBlockLists[idx], blk);
 
-	//(case)lw all block fe el page free: free el page.
-	int blkC=PAGE_SIZE/blk_size;
-	// the blk count in the page is same as number fo free blocks free up the page.
-	if (pgf->num_of_free_blocks==blkC) {
-		uint32 pgtoF=to_page_va(pgf);
-		// remove all blk in the freeBlockList.
-		for (uint32 i=0; i<PAGE_SIZE; i+=blk_size) {
-			struct BlockElement *blk=(struct BlockElement*)(pgtoF+i);
-			LIST_REMOVE(&freeBlockLists[idx],blk);
+	// If all blocks are free, return page
+	int total_blocks = PAGE_SIZE / blk_size;
+	if (pg->num_of_free_blocks == total_blocks)
+	{
+		uint32 pg_va = to_page_va(pg);
+		for (uint32 i = 0; i < PAGE_SIZE; i += blk_size)
+		{
+			struct BlockElement *b = (struct BlockElement *)(pg_va + i);
+			LIST_REMOVE(&freeBlockLists[idx], b);
 		}
-		// reset page info.
-		pgf->block_size = 0;
-		pgf->num_of_free_blocks = 0;
-		// add it to the available pages.
-		LIST_INSERT_TAIL(&freePagesList,pgf);
-		// make it available in memory.
-		return_page((uint32*)to_page_va(pgf));
+
+		pg->block_size = 0;
+		pg->num_of_free_blocks = 0;
+
+		LIST_INSERT_TAIL(&freePagesList, pg);
+		return_page((uint32 *)pg_va);
 	}
-	//panic("free_block() Not implemented yet");
 }
 
 //==================================================================================//
